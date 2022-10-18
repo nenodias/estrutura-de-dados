cf0969ec32bb504d53d4161a318c01576c6dddfb
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-10-18 14:37:26 -0300

    Formating file

--- 02-recursividade/main.c
+++ 02-recursividade/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fatorial(int numero){
-    //Algoritmos recursivos precisam de um termo de saída para ficar em um loop/laço infinito
-    if(numero == 0 || numero == 1){
+int fatorial(int numero)
+{
+    // Algoritmos recursivos precisam de um termo de saída para ficar em um loop/laço infinito
+    if (numero == 0 || numero == 1)
+    {
         return 1;
     }
-    return numero * fatorial(numero -1);
+    return numero * fatorial(numero - 1);
 }
-
 
 int main()
 {
