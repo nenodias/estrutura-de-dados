102f7efecd6a0ec180f510d24c09e40ca715c025
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-10-18 14:29:26 -0300

    Adicionando algoritmo fatorial recursivo

---
+++ 02-recursividade/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fatorial(int numero){
+    //Algoritmos recursivos precisam de um termo de saída para ficar em um loop/laço infinito
+    if(numero == 0 || numero == 1){
+        return 1;
+    }
+    return numero * fatorial(numero -1);
+}
+
+
+int main()
+{
+    printf("%d", fatorial(5));
+    return 0;
+}
