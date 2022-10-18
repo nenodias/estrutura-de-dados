394e470fda3068d568431cd5901b36e088a6842f
nenodias <horacio.dias92@gmail.com>
2022-10-18 13:49:11 -0300

    Initial commit

---
+++ passagem_de_valor/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void passagemDeValorPorCopia(int valor){
+    printf("passagemDeValorPorCopia\n");
+    printf("Antes: %d\n", valor);
+    valor = 2;
+    printf("Depois: %d\n", valor);
+}
+
+void passagemDeValorPorReferencia(int* valor){
+    printf("passagemDeValorPorReferencia\n");
+    printf("Antes: %d\n", *valor);
+    *valor = 2;
+    printf("Depois: %d\n", *valor);
+}
+
+void exibeVetor(int vetor[]){
+    printf("[");
+    //Pegando o tamanho do array do tipo int
+    int size = sizeof(vetor) / sizeof(int);
+    for (int i = 0; i <= size; i++)
+    {
+        printf ("%d,", vetor[i]);
+    }
+    printf("]\n");
+}
+
+void passagemDeArrayPorReferencia(int vetor[])
+{
+    exibeVetor(vetor);
+    vetor[0] = 0;
+}
+
+int main()
+{
+    int valor = 10;
+    //Ao passar o valor por cópia a variável NÃO é alterado fora da função
+    passagemDeValorPorCopia(valor);
+    printf("Main: %d\n", valor);
+    int* ponteiro_valor = &valor;
+    //Ao passar o valor por referência a variável é alterado fora da função
+    passagemDeValorPorReferencia(ponteiro_valor);
+    printf("Main: %d\n", valor);
+
+    //Exemplo de que todo vetor/array é passado por referência
+    int vetor[] = {10, 20, 30};
+    printf("Antes: passagemDeArrayPorReferencia\n");
+    passagemDeArrayPorReferencia(vetor);
+    printf("Depois: passagemDeArrayPorReferencia\n");
+    exibeVetor(vetor);
+    printf("Antes: passagemDeValorPorReferencia\n");
+    passagemDeValorPorReferencia(vetor);
+    exibeVetor(vetor);
+    return 0;
+}
---
+++ pilha/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TPilha{
+    struct TPilha* proximo;
+    int valor;
+};
+typedef struct TPilha TPilha;
+
+TPilha* criarPilha(){
+    TPilha* registro = (struct TPilha*) malloc(sizeof(struct TPilha));
+    registro->proximo = NULL;
+    registro->valor = 0;
+    return registro;
+}
+
+TPilha* criarPilhaComValor(int valor){
+    TPilha* registro = criarPilha();
+    registro->valor = valor;
+    return registro;
+}
+
+TPilha* empilhar(TPilha* pilha, int valor){
+    if(pilha != NULL){
+        struct TPilha* topo = criarPilhaComValor(valor);
+        topo->proximo = pilha;
+        return topo;
+    }
+    return NULL;
+}
+
+TPilha* desempilhar(TPilha* pilha){
+    if(pilha != NULL){
+        TPilha* proximo = pilha->proximo;
+        free(pilha);
+        return proximo;
+    }
+    return NULL;
+}
+
+void exibirPilha(TPilha* pilha){
+    if(pilha != NULL){
+        printf("Pilha[");
+        while(1){
+            printf("%d,",pilha->valor);
+            if(pilha->proximo == NULL){
+                break;
+            } else {
+                pilha = pilha->proximo;
+            }
+        };
+        printf("]\n");
+    } else {
+        printf("NULL\n");
+    }
+}
+
+int main()
+{
+    TPilha* pilha = criarPilhaComValor(5);
+    exibirPilha(pilha);
+    pilha = empilhar(pilha, 4);
+    exibirPilha(pilha);
+    pilha = empilhar(pilha, 3);
+    exibirPilha(pilha);
+    pilha = empilhar(pilha, 2);
+    exibirPilha(pilha);
+    pilha = empilhar(pilha, 1);
+    exibirPilha(pilha);
+
+    pilha = desempilhar(pilha);
+    exibirPilha(pilha);
+    pilha = desempilhar(pilha);
+    exibirPilha(pilha);
+    pilha = desempilhar(pilha);
+    exibirPilha(pilha);
+    pilha = desempilhar(pilha);
+    exibirPilha(pilha);
+    pilha = desempilhar(pilha);
+    exibirPilha(pilha);
+
+    return 0;
+}
