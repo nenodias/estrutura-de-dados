fec2874d449a5474137e38b8d99992a3446fe45c
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-10-19 15:50:41 -0300

    Formating c

--- 13-ponteiro_funcao/main.c
+++ 13-ponteiro_funcao/main.c
@@ -20,23 +20,30 @@ int main()
     printf("Digite o valor de b:");
     scanf("%d", &b);
     printf("Qual operação[+,-]:");
-    //Resolvendo bug com buffer do teclado
-    while(opcao == '\n'){
+    // Resolvendo bug com buffer do teclado
+    while (opcao == '\n')
+    {
         scanf("%c\n", &opcao);
     }
-    if(opcao == '+' || opcao == '-'){
+    if (opcao == '+' || opcao == '-')
+    {
         // Definindo um ponteiro para a função que retorno int e recebe dois ints
         int (*funcao_operacao)(int, int) = NULL;
-        if(opcao == '+') {
+        if (opcao == '+')
+        {
             // Pegando o endereco da funcao
             funcao_operacao = &somar;
-        } else if(opcao == '-'){
+        }
+        else if (opcao == '-')
+        {
             // Pegando o endereco da funcao
             funcao_operacao = &subtrair;
         }
         int resultado = (*funcao_operacao)(a, b);
         printf("Resultado da operação %d %c %d = %d", a, opcao, b, resultado);
-    } else {
+    }
+    else
+    {
         printf("Operação %c desconhecida.", opcao);
     }
     return 0;
